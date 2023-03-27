c5dccfe747bbd910e8c48de97ce7867d25549d6f
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2023-03-27 20:33:54 +0200

    Had to review idea with separate @BeforeMethod - steps in it was causing app to crash before the test steps were performed with success; added assertions for the first test in the series

--- src/test/java/com/testset/akaraskiewicz/library/Utilities.java
+++ src/test/java/com/testset/akaraskiewicz/library/Utilities.java
@@ -1,7 +1,6 @@
 package com.testset.akaraskiewicz.library;
 
 import com.google.common.collect.ImmutableMap;
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -43,15 +42,6 @@ public class Utilities {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @BeforeMethod (onlyForGroups = "AlertDialogs")
-    public void alertDialogsSetup() throws MalformedURLException {
-        appiumConfig();
-        System.out.println("alertDialogSetupDone");
-
-        //go to the Alert Dialogs
-        driver.findElement(AppiumBy.accessibilityId("App")).click();
-        driver.findElement(AppiumBy.accessibilityId("Alert Dialogs")).click();
-    }
 
     public void longPressAction(WebElement elementToBeLongPressed) {
         ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
--- src/test/java/com/testset/akaraskiewicz/tests/AlertDialogsExercise.java
+++ src/test/java/com/testset/akaraskiewicz/tests/AlertDialogsExercise.java
@@ -2,12 +2,38 @@ package com.testset.akaraskiewicz.tests;
 
 import com.testset.akaraskiewicz.library.Utilities;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AlertDialogsExercise extends Utilities {
+
+    public void alertDialogsSetup() {
+        //go to the Alert Dialogs -> having these steps in @BeforeMethod crashes the app
+        driver.findElement(AppiumBy.accessibilityId("App")).click();
+        driver.findElement(AppiumBy.accessibilityId("Alert Dialogs")).click();
+    }
     @Test (groups = "AlertDialogs")
     public void dialogWithAMessageTest() {
+        alertDialogsSetup();
+
         driver.findElement(AppiumBy.accessibilityId("OK Cancel dialog with a message")).click();
 
+        //verify if the pop-up exists and check its content (icon, title and buttons)
+        boolean isPopUpDisplayed = driver.findElement(By.id("android:id/parentPanel")).isDisplayed();
+        Assert.assertTrue(isPopUpDisplayed);
+
+        boolean isIconDisplayed = driver.findElement(By.id("android:id/icon")).isDisplayed();
+        Assert.assertTrue(isIconDisplayed);
+
+        String popUpTitleText = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(popUpTitleText, "Lorem ipsum dolor sit aie consectetur adipiscing\n" +
+                "Plloaso mako nuto siwuf cakso dodtos anr koop.");
+
+        boolean isCancelBtnDisplayed = driver.findElement(By.xpath("//android.widget.Button[@text = 'Cancel']")).isDisplayed();
+        Assert.assertTrue(isCancelBtnDisplayed);
+
+        boolean isOkBtnDisplayed = driver.findElement(By.xpath("//android.widget.Button[@text = 'OK']")).isDisplayed();
+        Assert.assertTrue(isOkBtnDisplayed);
     }
 }
